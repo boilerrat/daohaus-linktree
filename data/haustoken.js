0a867b4e8c78a6dbde6589f20d9fa7eb475578bb
boilerrat <128boilerrat@gmail.com>
2023-05-07 21:40:06 -0400

    haus token added

---
+++ data/haustoken.js
@@ -0,0 +1,19 @@
+const haustoken = [
+  {
+      title: "$HAUS on Gnosis",
+      url: "https://blockscout.com/xdai/mainnet/token/0xb0C5f3100A4d9d9532a4CfD68c55F1AE8da987Eb/",
+      emoji: "💱",
+  },
+  {
+      title: "$HAUS on Mainnet",
+      url: "https://etherscan.io/token/0xf2051511b9b121394fa75b8f7d4e7424337af687",
+      emoji: "💸",
+  }, 
+  {
+      title: "$HAUS on Optimism",
+      url: "https://optimistic.etherscan.io/address/0x01B8b6384298D4848E3BE63D4C9D17830EeE488A",
+      emoji: "🪙",
+  },
+];
+
+export default haustoken;

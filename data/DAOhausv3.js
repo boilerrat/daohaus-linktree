adb7b9592e0821c69c9e0c758a9051c2b8e9b778
earth2travis <travis.mccutcheon@gmail.com>
2023-05-16 11:55:50 -0500

    Add link to Simple Search

--- data/DAOhausv3.js
+++ data/DAOhausv3.js
@@ -1,35 +1,39 @@
 const DAOhausv3 = [
-    {
-        title: "Admin App",
-        url: "https://admin.daohaus.fun/",
-        emoji: "🏢",
-      },
-      {
-        title: "Data Dashboard",
-        url: "https://flipsidecrypto.xyz/santtee/dao-haus-dashboard-gP6ort",
-        emoji: "📊",
-      },          
-      {
-        title: "Docs",
-        url: "https://docs.daohaus.club/",
-        emoji: "📖",
-      },
-      {
-        title: "Subgraph",
-        url: "https://thegraph.com/explorer/subgraph?id=GfHFdFmiSwW1PKtnDhhcxhArwtTjVuMnXxQ5XcETF1bP&view=Overview",
-        emoji: "📈",
-      },       
-      {
-        title: "Summon App",
-        url: "https://summon.daohaus.fun/",
-        emoji: "🧙",
-      },
-      {
-        title: "Website",
-        url: "https://daohaus.club/",
-        emoji: "🌐",
-      },      
+  {
+    title: 'Admin App',
+    url: 'https://admin.daohaus.fun/',
+    emoji: '🏢',
+  },
+  {
+    title: 'Data Dashboard',
+    url: 'https://flipsidecrypto.xyz/santtee/dao-haus-dashboard-gP6ort',
+    emoji: '📊',
+  },
+  {
+    title: 'Docs',
+    url: 'https://docs.daohaus.club/',
+    emoji: '📖',
+  },
+  {
+    title: 'Subgraph',
+    url: 'https://thegraph.com/explorer/subgraph?id=GfHFdFmiSwW1PKtnDhhcxhArwtTjVuMnXxQ5XcETF1bP&view=Overview',
+    emoji: '📈',
+  },
+  {
+    title: 'Summon App',
+    url: 'https://summon.daohaus.fun/',
+    emoji: '🧙',
+  },
+  {
+    title: 'Simple Search',
+    url: 'https://simple-search.daohaus.club/',
+    emoji: '🔎',
+  },
+  {
+    title: 'Website',
+    url: 'https://daohaus.club/',
+    emoji: '🌐',
+  },
 ];
 
 export default DAOhausv3;
-  

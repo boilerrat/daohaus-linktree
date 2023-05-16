23c2efefeaa0819dc2dbddd2d674abec5bd7f6d6
earth2travis <travis.mccutcheon@gmail.com>
2023-05-16 11:57:57 -0500

    Update link to Baal Docs

--- data/links.js
+++ data/links.js
@@ -1,26 +1,24 @@
 const links = [
   {
-    title: "Baal Docs",
-    url: "https://baal-docs.vercel.app/",
-    emoji: "📝",
-  }, 
+    title: 'Baal Docs',
+    url: 'https://moloch.daohaus.fun/',
+    emoji: '📝',
+  },
   {
-    title: "Baal Github",
-    url: "https://github.com/Moloch-Mystics/Baal",
-    emoji: "👹",
-   }, 
-   {  
-    title: "DAOhaus Developer Docs",
-    url: "https://docs.daohaus.club/developer",
-    emoji: "📖",
-    },
-    {
-      title: "Github",
-      url: "https://github.com/HausDAO",
-      emoji: "👨‍💻",
-    }, 
+    title: 'Baal Github',
+    url: 'https://github.com/Moloch-Mystics/Baal',
+    emoji: '👹',
+  },
+  {
+    title: 'DAOhaus Developer Docs',
+    url: 'https://docs.daohaus.club/developer',
+    emoji: '📖',
+  },
+  {
+    title: 'Github',
+    url: 'https://github.com/HausDAO',
+    emoji: '👨‍💻',
+  },
 ];
 
 export default links;
-  
-
